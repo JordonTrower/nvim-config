d7b84d0d339cd1669d5cc1e1c418b499eee28e42
JordonTrower <leopardclone@gmail.com>
2025-07-08 18:52:49 -0400

    Add c#

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "lua_ls", "ts_ls", "yamlls", "rust_analyzer" }
+local servers = { "html", "cssls", "clangd", "lua_ls", "ts_ls", "yamlls", "rust_analyzer", "csharp_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
